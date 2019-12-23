ce6b7c17ffb8eee103222af4cee80089ef832b46
Isaac Avilez <isaac1180@aol.com>
2019-12-23 22:57:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,16 @@
-class Student < User 
-  
+class Student < User
+
   attr_accessor :knowledge
-  
-  def initialize(knowledge)
+
+  def initialize
     @knowledge = []
-  end 
+  end
 
-  def learn
-  end 
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
+  def knowledge
+    @knowledge
+  end
 end
